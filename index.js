cb2f63ed7711d25073647f0c979893b6aec19c17
astur <astur@yandex.ru>
2018-08-16 18:01:28 +0300

    Fix: empty array from empty file

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
-module.exports = path => fs.readFileSync(path, 'utf8').split('\n');
+module.exports = path => fs
+    .readFileSync(path, 'utf8')
+    .split('\n')
+    .filter((v, _, a) => a.length > 1 || v.length > 0);
