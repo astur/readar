5cecd831d1ac851b01c72eae694f4a465ff8aab5
astur <astur@yandex.ru>
2018-08-16 17:56:42 +0300

    test for empty file

--- test.js
+++ test.js
@@ -5,10 +5,12 @@ const fs = require('fs');
 test.before(t => {
     fs.mkdirSync('_dir_');
     fs.writeFileSync('_file_', '111\n222\n333', 'utf8');
+    fs.writeFileSync('_empty_', '', 'utf8');
 });
 
 test('readar', t => {
     t.deepEqual(readar('_file_'), ['111', '222', '333']);
+    t.deepEqual(readar('_empty_'), []);
     const ed = t.throws(() => readar('_dir_'));
     t.is(ed.code, 'EISDIR');
     const eb = t.throws(() => readar('_bad_'));
@@ -18,4 +20,5 @@ test('readar', t => {
 test.after.always(t => {
     fs.rmdirSync('_dir_');
     fs.unlinkSync('_file_');
+    fs.unlinkSync('_empty_');
 });
